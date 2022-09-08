e73cf01e3a5eb62b512cd9fb2ad45b94201a1170
Equilibridix <adedejiaishatayo@gmail.com>
2022-09-08 04:56:17 -0700

    C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - prints onto screen 
+ *
+ * Return: zero
+ */
 int main(void)
 {
-	
 	char My_goal[] = "\"Programming is like building a multilingual puzzle";
 	
 	puts(My_goal); 
