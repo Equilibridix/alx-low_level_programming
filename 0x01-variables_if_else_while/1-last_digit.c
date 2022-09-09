ffb4e431d2e5badfd36fc0a34d1eef702292b576
Equilibridix <adedejiaishatayo@gmail.com>
2022-09-09 02:56:12 -0700

    Print last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,29 +3,28 @@
 #include <stdio.h>
 
 /**
- * main - Checks last digit
+ * main - Entry point
  *
- * Return: zero
+ * Return: zero(Successful)
  */
 int main(void)
 {
-	int n;
-
-	int 1;
+	int n, b;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	1 = n % 10;
-
-	if (1 > 5)
+	b = n  % 10;
+	if (b > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, 1);
-	} else if (1 == 0)
+		printf("Last digit of %d is %d and is greater than 5\n", n, b);
+	}
+	else if (b < 6 && b != 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, 1);
-	} else if (1 < 6 && 1 != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, b);
+	}
+	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 1);
+		printf("Last digit of %d is %d and is 0\n", n, b);
 	}
 	return (0);
 }
