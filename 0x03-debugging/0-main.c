f345aab6af63067528bca600e6cfda8da9df09f7
Equilibridix <adedejiaishatayo@gmail.com>
2022-09-14 07:11:27 -0700

    Debug and debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description - tests function that prints negative or positive
+ * Return: 0(succesful)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postive_or_negative(i);
+
+	return(0);
+}
