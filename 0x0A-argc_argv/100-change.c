49f1b69650d103dfb92d211ae74af7246e08a528
Equilibridix <adedejiaishatayo@gmail.com>
2022-09-30 04:13:09 -0700

    argc and argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -41,10 +41,10 @@ int coinConverter(int i)
 }
 
 /**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ * main - takes in exactly one argument for minimum coin count
+ * @argc: number of command line arguments
+ * @argv: array
+ * Return: 0 if exactly 1, otherwise 1.
  */
 int main(int argc, char *argv[])
 {
